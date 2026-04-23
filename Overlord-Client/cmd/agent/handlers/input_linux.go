666bbfac4d50e524ae28b651cd1e5bb3161404a5
KingKDot <why_are_you_looking_at_this@stoppeeping.com>
2026-04-23 12:20:23 -0500

    fix linux and mac

--- Overlord-Client/cmd/agent/handlers/input_linux.go
+++ Overlord-Client/cmd/agent/handlers/input_linux.go
@@ -7,6 +7,7 @@ package handlers
 func setCursorPos(x, y int32)        {}
 func sendMouseDown(button int)       {}
 func sendMouseUp(button int)         {}
+func sendMouseWheel(delta int32)     {}
 func sendKeyDown(vk uint16)          {}
 func sendKeyUp(vk uint16)            {}
 func sendTextInput(text string)      {}
